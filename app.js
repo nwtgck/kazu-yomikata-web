1c61ec5ba240923e08a3eb8bb5a8ffcfae26570d
Ryo Ota <nwtgck@gmail.com>
2017-11-20 17:08:17 +0900

    [add] Add kisus "溝", "澗", "正", "載", "極", "恒河沙", "阿僧祇", "那由他", "不可思議" and "無量大数"

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ angular.module("kazuYomikata", [])
         $scope.number = 4294967296;        
 
         // (from: https://www.wikiwand.com/ja/%E5%91%BD%E6%95%B0%E6%B3%95)
-        const kisus = ["万", "億", "兆", "京", "垓", "𥝱", "穣"]
+        const kisus = ["万", "億", "兆", "京", "垓", "𥝱", "穣", "溝", "澗", "正", "載", "極", "恒河沙", "阿僧祇", "那由他", "不可思議", "無量大数"]
 
         $scope.numberText = function(){
 
