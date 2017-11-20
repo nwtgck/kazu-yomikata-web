886d46fec968a4723d2c7550662627708b6dab5a
Ryo Ota <nwtgck@gmail.com>
2017-11-20 17:04:08 +0900

    [impl] Implement number to Japanese number text

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 angular.module("kazuYomikata", [])
     .controller('kazuCtrl', ['$scope', function($scope){
-        $scope.number = 4294967296;
+        $scope.number = 4294967296;        
+
+        // (from: https://www.wikiwand.com/ja/%E5%91%BD%E6%95%B0%E6%B3%95)
+        const kisus = ["万", "億", "兆", "京", "垓", "𥝱", "穣"]
 
         $scope.numberText = function(){
-            return $scope.number+"";
+
+            let outText     = "";
+            let num         = $scope.number;
+            for(let kisuIdx = kisus.length-1; kisuIdx >= 0; kisuIdx--){
+                console.log("hello, world");
+            
+                const diver = Math.pow(10, 4*(kisuIdx + 1))
+                const a = Math.floor(num / diver);
+                num = num % diver;
+                if(a != 0) {
+                    outText += `${a}${kisus[kisuIdx]}`;
+                }
+            }
+            return outText+num;
         };
     }]);
